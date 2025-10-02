63dc9366394d344a925daaeaf7f9f387d6fbad14
natnael <nsisay49@gmail.com>
2025-10-02 23:27:14 +0300

    admin auth

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 
 //middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 //routes
